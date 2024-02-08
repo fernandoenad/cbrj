fec34347df275a32a0d908819f19bfb83a258275
Fernando Enad <31582634+fernandoenad@users.noreply.github.com>
2024-02-08 14:53:49 +0800

    Update 2024_02_07_035455_add_employee_id_to_users_table.php

--- database/migrations/2024_02_07_035455_add_employee_id_to_users_table.php
+++ database/migrations/2024_02_07_035455_add_employee_id_to_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('employee_id');
+            
         });
     }
 };
